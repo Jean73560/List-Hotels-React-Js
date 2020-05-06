59468aa99b9a2a070d3ee7a167a5773490443409
Jean Pierre <60325650+Jean73560@users.noreply.github.com>
2020-05-06 09:14:42 -0500

    public site

---
+++ proyect-hotels/docs/precache-manifest.99946f4b0c24b833e30cf1092cc00a4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d829c8db77634eb873486403a9f8a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db36c9dcb8c76b5d1e5f",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "a698effc320aa93fa9c7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "db36c9dcb8c76b5d1e5f",
+    "url": "/static/js/2.2f22d8ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f22d8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a698effc320aa93fa9c7",
+    "url": "/static/js/main.07c81ee0.chunk.js"
+  },
+  {
+    "revision": "17f7ea839e3ba604697e",
+    "url": "/static/js/runtime-main.3436ed8d.js"
+  }
+]);
